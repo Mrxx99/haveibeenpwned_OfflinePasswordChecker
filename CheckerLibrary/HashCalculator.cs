fff91e9998c56c64be666eb36773a3f54f083e08
mrxx99 <mrxx99@googlemail.com>
2019-01-19 13:48:52 +0100

    added hash calulation and ConsoleUI

---
+++ CheckerLibrary/HashCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace CheckerLibrary
+{
+    public static class HashCalculator
+    {
+        public static string CalculateHashString(string input)
+        {
+            var bytes = CalculateHashBytes(input);
+
+            return BitConverter.ToString(bytes).Replace("-", string.Empty).ToLowerInvariant();
+        }
+
+        public static byte[] CalculateHashBytes(string input)
+        {
+            var asciiEncoder = new System.Text.ASCIIEncoding();
+
+            var bytes = asciiEncoder.GetBytes(input);
+
+            return CalculateHash(bytes);
+        }
+
+        private static byte[] CalculateHash(byte[] input)
+        {
+            var prov = SHA1.Create();
+            return prov.ComputeHash(input);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using CheckerLibrary;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("your password:");
+            var input = Console.ReadLine();
+            var hash = HashCalculator.CalculateHashString(input);
+            Console.WriteLine($"the hash is: {hash}");
+            Console.ReadLine();
+        }
+    }
+}
